52d5f9ef05529fee3d34a6c160f091465b269260
khuuvandoan <104571793+khuuvandoan@users.noreply.github.com>
2022-07-26 00:19:01 +0700

    Update antiddos-debian.sh

--- antiddos-debian.sh
+++ antiddos-debian.sh
@@ -6,7 +6,7 @@ iptables -N SAFEZONE
 iptables -N IN_DPI_RULES 
 iptables -N IN_CUSTOMRULES
 
-echo "Dropping bogus TCP pkts before conntrack occurs to save CPU and replicating in mangle table to ensure no rule interferes with our DROPs"
+echo "Bỏ pkts TCP không có thật trước khi xảy ra sự cố để tiết kiệm CPU và sao chép trong bảng mangle để đảm bảo không có quy tắc nào can thiệp vào DROP của chúng tôi"
 iptables -t raw -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
 iptables -t raw -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
 iptables -t raw -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
@@ -15,7 +15,7 @@ iptables -t raw -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
 iptables -t raw -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
 iptables -t raw -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP
 iptables -t raw -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
-iptables -t raw -A PREROUTING -p tcp --tcp-flags ALL ALL -m comment --comment "xmas pkts (xmas portscanners)" -j DROP
+iptables -t raw -A PREROUTING -p tcp --tcp-flags ALL ALL -m comment --comment "xmas pkts (trình quét cổng xmas)" -j DROP
 iptables -t raw -A PREROUTING -p tcp --tcp-flags ALL NONE -m comment --comment "null pkts (null portscanners)" -j DROP
 iptables -t raw -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
 iptables -t raw -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
@@ -27,11 +27,11 @@ iptables -t raw -A PREROUTING -s 192.0.2.0/24 -m comment --comment "BOGONS" -j D
 iptables -t raw -A PREROUTING -s 10.0.0.0/8 -m comment --comment "BOGONS" -j DROP
 iptables -t raw -A PREROUTING -s 0.0.0.0/8 -m comment --comment "BOGONS" -j DROP
 iptables -t raw -A PREROUTING -s 240.0.0.0/5 -m comment --comment "BOGONS" -j DROP
-iptables -t raw -A PREROUTING -s 127.0.0.0/8 ! -i lo -m comment --comment "Only lo iface can have an addr-range of 127.0.0.x/8" -j DROP
-echo "Using the mangle table to drop pkts before a routing decision is made also saves CPU resources"
+iptables -t raw -A PREROUTING -s 127.0.0.0/8 ! -i lo -m comment --comment "Chỉ  iface mới có thể có phạm vi bổ trợ là 127.0.0.x / 8" -j DROP
+echo "Sử dụng bảng mangle để thả pkts trước khi đưa ra quyết định định tuyến cũng giúp tiết kiệm tài nguyên CPU"
 iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
-iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -m comment --comment "DROP new packets that don't present the SYN flag" -j DROP
-iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -m comment --comment "DROP new pkts that have malformed mss values" -j DROP
+iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -m comment --comment "DROP các gói mới không hiển thị cờ SYN" -j DROP
+iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -m comment --comment "DROP các pkts mới có giá trị mss không đúng định dạng" -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
@@ -40,12 +40,12 @@ iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL ALL -m comment --comment "xmas pkts (xmas portscanners)" -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL ALL -m comment --comment "xmas pkts (trình quét cổng xmas)" -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -m comment --comment "null pkts (null portscanners)" -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
 iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -m comment --comment "limit RST pkts (half-handshakes)" -j DROP
+iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -m comment --comment "giới hạn pkts RST (nửa bắt tay)" -j DROP
 iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -m comment --comment "BOGONS" -j DROP
 iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -m comment --comment "BOGONS" -j DROP
 iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -m comment --comment "BOGONS" -j DROP
@@ -74,4 +74,4 @@ iptables -A SAFEZONE -s x.x.x.x/32 -m comment --comment "allow-ingress-from-xxx-
 iptables -A SAFEZONE -s x.x.x.x/32 -m comment --comment "allow-ingress-from-xxx-hq" -j ACCEPT
 iptables -A SAFEZONE -m comment --comment "JUMP back to IN_CUSTOMRULES chain" -j RETURN
 
-echo "Firewall configuration successfully applied. If you would like to undo this config, edit the script and replace -A with -D."
+echo "Đã áp dụng thành công cấu hình tường lửa. Nếu bạn muốn hoàn tác cấu hình này, hãy chỉnh sửa tập lệnh và thay thế -A bằng -D"
